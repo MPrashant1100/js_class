2564b7ac275dea75ba0143e6c00f25704f896813
MPrashant1100 <prashantmishra1100@gmail.com>
2024-11-14 21:06:19 +0530

    Var Const Let

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Prashant")
+// const studentName = "Prashant"
+// let studentAddress = "Lucknow"
+// var studentId = "123"
+// isPass = true
+
+
+// var studentId = "12"
+// console.log(studentId)
+
