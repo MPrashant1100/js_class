46586130cff7e5faa0eaa074910edf37630f3d25
MPrashant1100 <prashantmishra1100@gmail.com>
2024-11-15 21:24:19 +0530

    Class 2 done

--- index.js
+++ index.js
@@ -3,14 +3,60 @@
 // var studentId = "123"
 // isPass = true
 
-
 // var studentId = "12"
 // console.log(studentId)
 
-// Data Type 
-   let age = 25    // Number 
-   let name = "Prashant"   // String
-   let isFail = false    // boolean
-   let address = null
+// Data Type
+// let age = 25    // Number
+// let name = "Prashant"   // String
+// let isFail = false    // boolean
+// let address = null
 
-   console.log(typeof address)
+// console.log(typeof address)
+// console.log(typeof age)
+
+// Primitive data type
+
+// console.log(3 > 5)  // Boolean
+// console.log( 3 == '3') // only compare
+// console.log( 3 === '3')  // type check and compare
+
+// let a = 3
+// let b = "4"
+
+// let sum = a + b
+
+// console.log(sum)
+
+// Non - Primitive Data Type
+
+// 1.1 Object - ( Key - Value Pair )
+
+let students = [
+  {
+    name: "Parshant",
+    age: 25,
+    isAdmin: true,
+    profile: "Full Stack Engineer",
+  },
+  {
+   name: "Tanmay",
+   age: 22,
+   isAdmin: true,
+   profile: "Frontend Engineer",
+ },
+];
+
+console.log(students[1].age)
+
+//console.log("This my student", student)
+// console.log("This is student Name", student.name , student.age, student.isAdmin, student.profile)
+
+// 1.2 Array - Array can Contain value of different type.
+
+// let colors = ["red", "green", "blue"]; // 0 ->  n-1
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+// console.log(colors.length);
