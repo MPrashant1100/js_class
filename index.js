e8375e118035e19bfb0f9ac225d71aa9bbbcd5dd
MPrashant1100 <prashantmishra1100@gmail.com>
2024-11-20 21:31:53 +0530

    Project 1 done

--- index.js
+++ index.js
@@ -1,62 +1,53 @@
-// const studentName = "Prashant"
-// let studentAddress = "Lucknow"
-// var studentId = "123"
-// isPass = true
+// // 1. Minimum two value ( var )
+// // 2. operator 
+// // 3. Result 
 
-// var studentId = "12"
-// console.log(studentId)
+// let num1 = 1.2
+// let num2 = 9.5
+// let operator = '*'
 
-// Data Type
-// let age = 25    // Number
-// let name = "Prashant"   // String
-// let isFail = false    // boolean
-// let address = null
+// let result 
 
-// console.log(typeof address)
-// console.log(typeof age)
+// // if-else 
+// if ( operator === '+') {
+//    result  = num1 + num2
+// } else if ( operator === '-') {
+//    result = num1 - num2
+// } else if ( operator === '*') {
+//    result = num1 * num2 
+// } else if ( operator === '/') {
+//    if( num2 === '0') {
+//       result = "Error: Divison by Zero"
+//    } else {
+//       result = num1 / num2
+//    } 
+// } else {
+//     result = "Invalid operator"  
+// }
 
-// Primitive data type
+// console.log("Result", result)
 
-// console.log(3 > 5)  // Boolean
-// console.log( 3 == '3') // only compare
-// console.log( 3 === '3')  // type check and compare
+function calculate() {
+   let num1 = parseInt(document.getElementById("num1").value)
+   let num2 = parseInt(document.getElementById("num2").value)
+   let operator = document.getElementById("operator").value
 
-// let a = 3
-// let b = "4"
+   let result
 
-// let sum = a + b
-
-// console.log(sum)
-
-// Non - Primitive Data Type
-
-// 1.1 Object - ( Key - Value Pair )
-
-let students = [
-  {
-    name: "Parshant",
-    age: 25,
-    isAdmin: true,
-    profile: "Full Stack Engineer",
-  },
-  {
-   name: "Tanmay",
-   age: 22,
-   isAdmin: true,
-   profile: "Frontend Engineer",
- },
-];
-
-console.log(students[1].age)
-
-//console.log("This my student", student)
-// console.log("This is student Name", student.name , student.age, student.isAdmin, student.profile)
-
-// 1.2 Array - Array can Contain value of different type.
-
-// let colors = ["red", "green", "blue"]; // 0 ->  n-1
-// console.log(colors[0]);
-// console.log(colors[1]);
-// console.log(colors[2]);
-
-// console.log(colors.length);
+   if ( operator === '+') {
+         result  = num1 + num2
+      } else if ( operator === '-') {
+         result = num1 - num2
+      } else if ( operator === '*') {
+         result = num1 * num2 
+      } else if ( operator === '/') {
+         if( num2 === '0') {
+            result = "Error: Divison by Zero"
+         } else {
+            result = num1 / num2
+         } 
+      } else {
+          result = "Invalid operator"  
+      }
+     document.getElementById("result").textContent = "Result: " + result 
+}
